b3f694083e8c7aedaff7cc484ddea82e37aeae0b
nole23 <nole0223@gmail.com>
2024-12-07 18:49:17 +0100

    Configure Git actions. Server setup test. Closes #6

--- server/uks/settings.py
+++ server/uks/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +91,8 @@ elif os.environ.get('DJANGO_ENV') == 'test':
     env_file = '.env.test'
 
 # Učitaj varijable iz odgovarajuće .env datoteke
-config.read(env_file)  # Dodajemo ovo da bismo učitali specifičnu .env datoteku
+from dotenv import load_dotenv
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, env_file))
 
 # Učitavanje varijabli iz odgovarajuće .env datoteke
 DATABASES = {
